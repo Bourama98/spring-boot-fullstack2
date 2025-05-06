eb639431b6355fec91e0cc9930fb44997556ae25
Bourama98 <mangara98@hotmail.com>
2025-05-05 17:19:57 -0700

    I updated Java version to 24 in both pom.xml and backen-ci.yml file

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
-        //Test
+        //Test merging branch
         System.out.println("Testing");
     }
     @Bean
